f76a31baa2e8c692e2678e69b87698d14eb080c4
TycheBooker <tihana.bihler@gmail.com>
2018-08-13 15:32:15 +0200

    textureManager -> assetManager

--- Memory-CPP/TextureManager.cpp
+++ Memory-CPP/TextureManager.cpp
@@ -32,7 +32,41 @@ sf::Texture & TextureManager::getTexture(std::string const & filename)
   else
   {
     sf::Texture & texture = textureMap[filename];
-	texture.loadFromFile(GRAPHICS_PATH + filename);
+	  texture.loadFromFile(GRAPHICS_PATH + filename);
     return texture;
+  }
+}
+
+sf::Font & TextureManager::getTexture(std::string const & filename)
+{
+  std::map<std::string, sf::Font> & fontMap = singletonInstance->managerFonts;
+
+  std::map<std::string, sf::Font>::iterator pairFound = fontMap.find(filename);
+  if (pairFound != fontMap.end())
+  {
+    return pairFound->second;
+  }
+  else
+  {
+    sf::Font & font = fontMap[filename];
+	  font.loadFromFile(FONTS_PATH + filename);
+    return font;
+  }
+}
+
+sf::SoundBuffer & TextureManager::getSoundBuffer(std::string const & filename)
+{
+  std::map<std::string, sf::SoundBuffer> & soundBufferMap = singletonInstance->managerSoundBuffers;
+
+  std::map<std::string, sf::SoundBuffer>::iterator pairFound = soundBufferMap.find(filename);
+  if (pairFound != soundBufferMap.end())
+  {
+    return pairFound->second;
+  }
+  else
+  {
+    sf::SoundBuffer & soundBuffer = soundBufferMap[filename];
+	  soundBuffer.loadFromFile(AUDIO_PATH + filename);
+    return soundBuffer;
   }
 }
--- Memory-CPP/TextureManager.h
+++ Memory-CPP/TextureManager.h
@@ -2,20 +2,27 @@
 #include <map>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 #define GRAPHICS_PATH "data/graphics/"
+#define FONTS_PATH "data/fonts/"
+#define AUDIO_PATH "data/audio/"
 
 class TextureManager
 {
 public:
   static TextureManager * getInstance();
   static sf::Texture & getTexture(std::string const & filename);
+  static sf::Font & getFont(std::string const & filename);
+  static sf::SoundBuffer & getSoundBuffer(std::string const & filename);
 
 private:
   TextureManager();
   ~TextureManager();
 
   std::map<std::string, sf::Texture> managerTextures;
+  std::map<std::string, sf::Font> managerFonts;
+  std::map<std::string, sf::SoundBuffer> managerSoundBuffers;
   static TextureManager * singletonInstance;
 };
 
